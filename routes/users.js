fd8cd8403aed53b2edcbdf5290f0b29eea3bbb29
Mike Elsmore <mike@elsmore.me>
2015-06-01 14:34:43 +0100

    Add ability to review speakers

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router  = express.Router();
 var user    = require(__dirname + '/../lib/user');
+var config  = require(__dirname + '/../config');
+var talkDb  = require(__dirname + '/../lib/talk')(config.eventDB);
 var utils   = require(__dirname + '/../lib/utils');
 var _       = require('lodash');
 var extend  = require('util')._extend;
@@ -68,8 +70,43 @@ router.post('/', function(req, res) {
   // res.render('user/profile', { title: 'Your speaker profile', user: req.user, possibleAvatars: {} })
 });
 
+// List users§
 router.get('/list', utils.ensureAdmin, function(req, res) {
-  res.send('{fuck:you}');
+  // View options
+  var options = {
+    include_docs: true
+  };
+
+  // Grab the talks
+  user.view('user', 'byemail', options, function(err, data) {
+    res.render('user/list', { title: 'Users', users: data.rows });
+  });
+});
+
+// User details
+router.get('/:id', function(req, res) {
+
+  // The User ID
+  var userId = req.param('id');
+
+  // Get the talk doc
+  user.get(userId, function(err, userdata) {
+
+    var userId = userdata.id;
+
+    // View options
+    var options = {
+      include_docs: true,
+      key: userId
+    };
+    // Grab the talks
+    talkDb.view('user', 'byid', options, function(err, talkdata) {
+
+      res.render('user/profile-admin', { title:userdata.name, user: userdata, talks: talkdata.rows})
+
+    });
+
+  });
 });
 
 module.exports = router;
