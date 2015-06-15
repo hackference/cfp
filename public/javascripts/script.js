2f7cf0a7195760930486cdc16f11a00926c8f560
Mike Elsmore <mike@elsmore.me>
2015-06-15 15:04:06 +0100

    Fix linting issues

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
 
-  $('.talk-type').on('change', function(){
+  $('.talk-type').on('change', function() {
     var showAll = 0;
-    $('.talk-type').each(function(){
-      if($(this).is(':checked')) {
+    $('.talk-type').each(function() {
+      if ($(this).is(':checked')) {
         $('.row.' + $(this).val()).show();
       } else {
         $('.row.' + $(this).val()).hide();
         showAll++;
       }
     });
+
     if (showAll == $('.talk-type').length) {
-      $('.talk-type').each(function(){
+      $('.talk-type').each(function() {
         $('.row.' + $(this).val()).show();
       });
     }
